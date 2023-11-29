9f8f9c04d29ffa9a6fd7154cefbfecbb848abda4
ip.osinga <ip.osinga@alfa-college.nl>
2023-11-29 10:24:51 +0100

    v2023-11-29

--- script.js
+++ script.js
@@ -21,9 +21,31 @@ function generateProblem()
     random_index_right = Math.floor(Math.random() * 7);
     unit_left.innerHTML = eenheden[random_index_left] + "<sup>" + dimension + "</sup>";
     unit_right.innerHTML = eenheden[random_index_right]+ "<sup>"+ dimension + "</sup>";
+    problem.value = (Math.random() * 1000).toFixed(3);
 }
 
 function checkSolution()
 {
-    alert("check doet het ook");
+    
+    let verm_factor = Math.pow(10,selected_dim.value);
+    //ik ben alleen geinteresseerd in het aantal stappen tussen de eenheden,
+    //door Math.abs wordt een negatief positief gemaakt
+    let aantal_stappen = Math.abs(random_index_left - random_index_right);
+    
+    //als de eenheid links groter is dan de eenheid rechts,
+    //dan wordt het antwoord groter, oftewel de komma moet naar rechts
+    //andersom: als de eenheid links kleiner is dan de eenheid rechts,
+    //dan wordt het antwoord kleiner, oftewel de komma moet naar links
+    let correct_answer = 3.14;
+    if(random_index_left > random_index_right)
+    {
+        //het antwoord wordt groter
+        correct_answer = problem.value * Math.pow(verm_factor, aantal_stappen);
+    }
+    else
+    {
+        //het antwoord wordt kleiner
+        correct_answer = problem.value / Math.pow(verm_factor, aantal_stappen);
+    }
+    alert("verm_factor = " + verm_factor + " en het aantal stappen = " + aantal_stappen + " het goede antwoord is: " + correct_answer );
 }
